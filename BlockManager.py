7c64c49b18a15f4724539aaa2a8c370273145aa9
Flavio Francisco <flaviofrancis@msn.com>
2022-11-17 16:30:33 +0100

    Bug fix: check in empty file.

--- BlockManager.py
+++ BlockManager.py
@@ -33,6 +33,8 @@ class BlockManager:
         with open(file_name, encoding='utf-8') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for row in csv_reader:
+                if len(row) == 0:
+                    continue
                 if screen_name in row[DefaultValues.SCREEN_NAME]:
                     return True
 
